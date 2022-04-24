4c440942dc7f4960ebbce3a7308547450aba8d51
katiedoherty <katie.doherty@mycit.ie>
2022-04-24 15:39:22 +0000

    configuring he project

--- server/server.js
+++ server/server.js
@@ -47,7 +47,37 @@ mongoose.connect(process.env.DATABASE_ACCESS, () => console.log("database connec
 // var foodcards = mongoose.model('foodcards',foodcardsSchema);
 //Data parsing
 app.use(express.json());
-app.use(cors())
+app.use(cors({
+    origin: function(origin, callback){
+
+    if(!origin) return callback(null, true);
+    
+     
+    if(allowedOrigins.indexOf(origin) === -1){
+    
+     
+    var msg = 'The CORS policy for this site does not ' +
+    
+     
+    'allow access from the specified Origin.';
+    
+     
+    return callback(new Error(msg), false);
+    
+     
+    }
+    
+     
+    return callback(null, true);
+    
+     
+    },
+     
+    exposedHeaders: ['Content-Length', 'X-Foo', 'X-Bar'],
+    credentials: true,
+    
+     
+    }));
 app.use(express.urlencoded({extended:false}));
 
 //HTTP request logger
